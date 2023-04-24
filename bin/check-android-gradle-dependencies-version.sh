e33d454b4eca5c9e4bd5c8c12885f4b901de14df
jun-wu-tw <jun.wu@thoughtworks.com>
2023-04-24 11:25:05 +0800

    Improve the file path usage for running command in specific folder

--- bin/check-android-gradle-dependencies-version.sh
+++ bin/check-android-gradle-dependencies-version.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 SUFFIX_REGEX=$1
+FILE_PATH=$2
 
 if [ "$SUFFIX_REGEX" == "" ]
 then
@@ -8,15 +9,15 @@ then
     exit 1
 fi
 
-API_SNAPSHOT_DEPENDENCIES_COUNT=$(./gradlew :app:dependencies --configuration api | awk '/'"$SUFFIX_REGEX"'/' | wc -l)
-IMPLEMENTATION_SNAPSHOT_DEPENDENCIES_COUNT=$(./gradlew :app:dependencies --configuration implementation | awk '/'"$SUFFIX_REGEX"'/' | wc -l)
+API_SNAPSHOT_DEPENDENCIES_COUNT=$($FILE_PATH/gradlew -p $FILE_PATH :app:dependencies --configuration api | awk '/'"$SUFFIX_REGEX"'/' | wc -l)
+IMPLEMENTATION_SNAPSHOT_DEPENDENCIES_COUNT=$($FILE_PATH/gradlew -p $FILE_PATH :app:dependencies --configuration implementation | awk '/'"$SUFFIX_REGEX"'/' | wc -l)
 SNAPSHOT_DEPENDENCIES_COUNT=`expr $API_SNAPSHOT_DEPENDENCIES_COUNT + $IMPLEMENTATION_SNAPSHOT_DEPENDENCIES_COUNT`
 
 if [ "$SNAPSHOT_DEPENDENCIES_COUNT" -gt "0" ]
 then
   printf "*** %s SNAPSHOT dependencies detected ***\n---\n" "$SNAPSHOT_DEPENDENCIES_COUNT"
-  echo $(./gradlew :app:dependencies --configuration api | awk '/'"$SUFFIX_REGEX"'/')
-  echo $(./gradlew :app:dependencies --configuration implementation | awk '/'"$SUFFIX_REGEX"'/')
+  echo $($FILE_PATH/gradlew -p $FILE_PATH :app:dependencies --configuration api | awk '/'"$SUFFIX_REGEX"'/')
+  echo $($FILE_PATH/gradlew -p $FILE_PATH :app:dependencies --configuration implementation | awk '/'"$SUFFIX_REGEX"'/')
   exit 1
 elif [ "$SNAPSHOT_DEPENDENCIES_COUNT" != "0" ]
 then
--- bin/check-service-maven-dependencies-version.sh
+++ bin/check-service-maven-dependencies-version.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 SUFFIX_REGEX=$1
+FILE_PATH=$2
 
 if [ "$SUFFIX_REGEX" == "" ]
 then
@@ -8,12 +9,12 @@ then
     exit 1
 fi
 
-SNAPSHOT_DEPENDENCIES_COUNT=$(mvn dependency:tree -Dscope=compile -Dincludes=:::\*$SUFFIX_REGEX | awk '/'"$SUFFIX_REGEX"'/' | wc -l)
+SNAPSHOT_DEPENDENCIES_COUNT=$(mvn -f $FILE_PATH dependency:tree -Dscope=compile -Dincludes=:::\*$SUFFIX_REGEX | awk '/'"$SUFFIX_REGEX"'/' | wc -l)
 
 if [ "$SNAPSHOT_DEPENDENCIES_COUNT" -gt "0" ]
 then
   printf "*** %s SNAPSHOT dependencies detected ***\n---\n" "$SNAPSHOT_DEPENDENCIES_COUNT"
-  echo $(mvn dependency:tree -Dscope=compile -Dincludes=:::\*$SUFFIX_REGEX | awk '/'"$SUFFIX_REGEX"'/')
+  echo $(mvn -f $FILE_PATH  dependency:tree -Dscope=compile -Dincludes=:::\*$SUFFIX_REGEX | awk '/'"$SUFFIX_REGEX"'/')
   exit 1
 elif [ "$SNAPSHOT_DEPENDENCIES_COUNT" != "0" ]
 then
